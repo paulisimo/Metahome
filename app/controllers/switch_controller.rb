ee7566e059d221eb250a8eea78a53009e31e9b8e
Topside <Sunshine@Topside.local>
2010-11-17 00:08:53 +0000

    added switch controller

---
+++ app/controllers/switch_controller.rb
@@ -0,0 +1,47 @@
+class SwitchController < ApplicationController
+  def index 
+    require 'rubygems'
+    Kernel::require "/Users/Sunshine/Dropbox/Projects/arduino/ruby-serialport-0.7.0/lib/serialport.rb"
+
+    #params for serial port
+
+    port_str = '/dev/tty.usbserial-A600dgPA'  #may be different for you
+    baud_rate = 9600
+    data_bits = 8
+    stop_bits = 1
+    parity = SerialPort::NONE
+    sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+   #lounge 
+     if params['on1']
+        sp.write "CMD:C011"
+      end
+      if params['off1']
+        sp.write "CMD:C010"
+      end
+   #topside
+    if params['on2']
+      sp.write "CMD:C021"
+    end
+    if params['off2']
+      sp.write "CMD:C020"
+    end
+    #bedroom
+     if params['on3']
+        sp.write "CMD:C031"
+      end
+      if params['off3']
+        sp.write "CMD:C030"
+      end
+    #bedroom
+     if params['on4']
+        sp.write "CMD:C041"
+      end
+      if params['off4']
+        sp.write "CMD:C040"
+      end
+  end 
+
+  def exit 
+  render :text => "Thanks" 
+  end 
+end
