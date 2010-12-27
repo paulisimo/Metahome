910317425b4dc3734e288df279c7692146c5c8e2
Topside <Sunshine@Topside.local>
2010-12-27 11:37:46 +0000

    made it send 2 on and off commands with a 0.5 sleep between

--- app/controllers/switch_controller.rb
+++ app/controllers/switch_controller.rb
@@ -38,36 +38,52 @@ class SwitchController < ApplicationController
    #lounge 
    if params['on1']
       write "CMD:C011"
+      sleep 0.5
+      write "CMD:C011"
       flash[:notice] = write("1 on")
     end
     if params['off1']
+      write "CMD:C010"
+      sleep 0.5
       write "CMD:C010"
       flash[:notice] = write("1 off")
     end
    #topside
     if params['on2']
       write "CMD:C021"
+      sleep 0.5
+      write "CMD:C021"
       flash[:notice] = write("2 on")
     end
     if params['off2']
+      write "CMD:C020"
+      sleep 0.5
       write "CMD:C020"
       flash[:notice] = write("2 off")
     end
     #bedroom
      if params['on3']
         write "CMD:C031"
+        sleep 0.5
+        write "CMD:C031"
         flash[:notice] = write("3 on")
     end
     if params['off3']
+      write "CMD:C030"
+      sleep 0.5
       write "CMD:C030"
       flash[:notice] = write("3 off")
     end
     #bedroom
    if params['on4']
       write "CMD:C041"
+      sleep 0.5
+      write "CMD:C041"
       flash[:notice] = write("4 on")
     end
     if params['off4']
+      write "CMD:C040"
+      sleep 0.5
       write "CMD:C040"
       flash[:notice] = write("4 off")
     end
