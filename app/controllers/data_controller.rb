03e4f70abf018d9ef583810c182e8e7d89c94a8b
Topside <Sunshine@Topside.local>
2010-12-31 01:26:36 +0000

    added light data, css tweaks, auto updating of data via jquery

---
+++ app/controllers/data_controller.rb
@@ -0,0 +1,13 @@
+class DataController < ApplicationController
+  def index
+    
+    sp = SerialPort.new("/dev/tty.usbserial-A600dgPA",9600,8,1,SerialPort::NONE)
+    sp_char = sp.gets
+    flash[:notice] = sp_char
+
+  end
+
+  def show
+  end
+
+end
--- app/controllers/switch_controller.rb
+++ app/controllers/switch_controller.rb
@@ -1,37 +1,3 @@
-class SerialConnection
-  def self.connection
-    @connection ||= establish_connection
-  end
-
-  def self.establish_connection
-    Kernel::require 'serialport.so'
-
-    #params for serial port
-
-    #port_str = '/dev/tty.usbserial-A600dgPA'  #may be different for you
-    port_str = '/dev/ttyUSB0'
-    baud_rate = 9600
-    data_bits = 8
-    stop_bits = 1
-    parity = SerialPort::NONE
-    
-    SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity).tap{ |conn|
-      #conn.write "\r\n"
-      #conn.flush
-      sleep 2     
-    }
-  end
-  
-  def self.send(data)
-    connection.write(data)
-    #while (received = connection.gets.strip) != data
-      #sleep 0.5
-    #end
-    #received
-    data
-  end
-end
-
 class SwitchController < ApplicationController
   def index 
     
@@ -101,3 +67,4 @@ class SwitchController < ApplicationController
 end
 
 #sp = SerialPort.new("/dev/ttyUSB0",9600,8,1,SerialPort::NONE)
+#sp = SerialPort.new('/dev/tty.usbserial-A600dgPA',9600,8,1,SerialPort::NONE)
