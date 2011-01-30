73531eb6bcc664e3bd57497dbe56bff5acb7891c
Paul Sims <Paul@saulpims-2.local>
2011-01-30 00:52:53 +0000

    tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class SerialConnection
 
     #params for serial port
 
-    port_str = '/dev/tty.usbserial-A600dgPA'  #may be different for you
-    #port_str = '/dev/ttyUSB0'
+    #port_str = '/dev/tty.usbserial-A600dgPA'  #may be different for you
+    port_str = '/dev/ttyUSB0'
     baud_rate = 9600
     data_bits = 8
     stop_bits = 1
